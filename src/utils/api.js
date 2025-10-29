e7f45e08e529037979399ae9d40be568204e9ca6
it20018900achini <achininirupama98@gmail.com>
2025-10-29 09:32:16 -0700

    order

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://pos-dsxh.onrender.com',
+  baseURL: 'https://pos-dsxh.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
