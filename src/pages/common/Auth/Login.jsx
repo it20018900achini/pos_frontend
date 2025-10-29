19cda2775b70741d1c304831d161cf48a55ee314
it20018900achini <achininirupama98@gmail.com>
2025-10-29 06:14:06 -0700

    add lkr

--- src/pages/common/Auth/Login.jsx
+++ src/pages/common/Auth/Login.jsx
@@ -264,9 +264,9 @@ const Login = () => {
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-border" />
                 </div>
-                <div className="relative flex justify-center text-sm">
+                {/* <div className="relative flex justify-center text-sm">
                   <span className="px-2 bg-card text-muted-foreground">Or continue with</span>
-                </div>
+                </div> */}
               </div>
             </div>
 
