7504a02408d76a9982a0d259f42f14369162e7db
it20018900achini <achininirupama98@gmail.com>
2025-11-11 11:24:21 -0800

    o pagin c

--- src/pages/cashier/cart/CartSummary.jsx
+++ src/pages/cashier/cart/CartSummary.jsx
@@ -14,36 +14,29 @@ const CartSummary = () => {
   const discountAmount = useSelector(selectDiscountAmount);
   const total = useSelector(selectTotal);
 
-  const formatPrice = (amount) =>
-    new Intl.NumberFormat("en-LK", { style: "currency", currency: "LKR" }).format(amount);
-
   return (
-    <div className="border-t bg-muted p-4 sticky bottom-0 z-10">
+    <div className="border-t bg-muted p-4">
       <div className="space-y-2 text-sm">
         <div className="flex justify-between">
           <span>Subtotal:</span>
-          <span>{formatPrice(subtotal)}</span>
+          <span>LKR {subtotal.toFixed(2)}</span>
         </div>
-        {tax > 0 && (
-          <div className="flex justify-between">
-            <span>Tax:</span>
-            <span>{formatPrice(tax)}</span>
-          </div>
-        )}
-        {discountAmount > 0 && (
-          <div className="flex justify-between">
-            <span>Discount:</span>
-            <span className="text-red-600">- {formatPrice(discountAmount)}</span>
-          </div>
-        )}
+        <div className="flex justify-between">
+          <span>Tax :</span>
+          <span>LKR {tax.toFixed(2)}</span>
+        </div>
+        <div className="flex justify-between">
+          <span>Discount:</span>
+          <span className="text-red-600">- LKR {discountAmount.toFixed(2)}</span>
+        </div>
         <Separator />
         <div className="flex justify-between text-lg font-bold">
           <span>Total:</span>
-          <span className="text-green-600">{formatPrice(total)}</span>
+          <span className="text-green-600">LKR {total?.toFixed(2)}</span>
         </div>
       </div>
     </div>
   );
 };
 
-export default React.memo(CartSummary);
+export default CartSummary;
