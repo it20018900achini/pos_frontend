29e3d2cc03603828c46b700b1a2dbb318ad6b7fb
Nalindadsn <nalindadsn@gmail.com>
2026-02-04 19:52:12 +0530

    u n15

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,11 +1,9 @@
 export const settings={
-    // url:"https://pos-frontend-sepia-chi.vercel.app/",
+    // url:"https://pos-dsxh.onrender.com/",
     businessName: "ABC Supermarket",
-     url:"https://pos-frontend-sepia-chi.vercel.app",
-     ws:"wss://pos-frontend-sepia-chi.vercel.app",
+     url:"https://pos-dsxh.onrender.com",
+     ws:"wss://pos-dsxh.onrender.com",
     //  url:"http://localhost:5000",
-    //  ws:"ws://localhost:5000",
     //  ws:"ws://localhost:5000",
      environment:"dev"
 }
-
