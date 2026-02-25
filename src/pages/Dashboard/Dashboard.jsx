bf84bb5978b0eb7ad6af21de0a62c7e1e3ca58e8
Nalindadsn <nalindadsn@gmail.com>
2026-02-25 11:12:46 +0530

    up

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import DashboardLayout from '../../components/Dashboard/DashboardLayout'
+import TodayOverview from '../Branch Manager/Dashboard/TodayOverview'
+import { useSelector } from 'react-redux';
+import { DashboardStats } from '../store/Dashboard';
 
 function Dashboard() {
+    const { userProfile } = useSelector((state) => state.user);
+
   return (
-    <div>Dashboard</div>
+    <div>
+      {userProfile?.user?.branch?.id ? <TodayOverview/>:<DashboardStats/>}
+      
+    </div>
   )
 }
 
