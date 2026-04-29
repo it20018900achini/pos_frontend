817a67e5fc94d8a25322c80330fa5aeb33689283
it20018900achini <your-email@github.com>
2026-04-29 16:43:41 +0530

    add up

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,9 +1,9 @@
 export const settings={
     // url:"https://pos-dsxh.onrender.com/",
     businessName: "ForkCodes",
-    //  url:"https://pos-dsxh.onrender.com",
-    //  ws:"wss://pos-dsxh.onrender.com",
-     url:"http://localhost:5000",
-     ws:"ws://localhost:5000",
+     url:"https://pos-dsxh.onrender.com",
+     ws:"wss://pos-dsxh.onrender.com",
+    //  url:"http://localhost:5000",
+    //  ws:"ws://localhost:5000",
      environment:"dev"
 }
